986539186a38c24775b8400b7497b17c1e625db1
Eslam Tohamy <117383087+eslamtohamy49@users.noreply.github.com>
2023-10-19 19:18:20 +0300

    Add files via upload

---
+++ Lab.4/Built-in_Functions.m
@@ -0,0 +1,17 @@
+% Create a vector of numbers
+vector = [12, 45, 23, 67, 9, 31, 54, 18];
+
+% Calculate the maximum value in the vector
+maximum_value = max(vector);
+
+% Calculate the minimum value in the vector
+minimum_value = min(vector);
+
+% Calculate the sum of all values in the vector
+sum_of_values = sum(vector);
+
+% Display the results
+fprintf('Vector of numbers: %s\n', mat2str(vector));
+fprintf('Maximum value: %d\n', maximum_value);
+fprintf('Minimum value: %d\n', minimum_value);
+fprintf('Sum of values: %d\n', sum_of_values);
